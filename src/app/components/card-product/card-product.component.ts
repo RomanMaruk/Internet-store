e17f7461de939b9b406b9e479b91d2fd5ab5badb
Roman Maruk <maruchok@gmail.com>
2023-08-03 19:39:35 +0300

    create product card

--- src/app/components/card-product/card-product.component.ts
+++ src/app/components/card-product/card-product.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IProduct } from 'src/app/models/product.model';
 
 @Component({
-  selector: 'app-card-product',
+  selector: 'app-card-product[product]',
   templateUrl: './card-product.component.html',
-  styleUrls: ['./card-product.component.scss']
+  styleUrls: ['./card-product.component.scss'],
 })
 export class CardProductComponent {
-
+  @Input() product!: IProduct;
 }
--- src/app/pages/list-products/list-products.component.ts
+++ src/app/pages/list-products/list-products.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IProduct } from 'src/app/models/product.model';
+import { ShopService } from 'src/app/services/shop.service';
 
 @Component({
   selector: 'app-list-products',
   templateUrl: './list-products.component.html',
-  styleUrls: ['./list-products.component.scss']
+  styleUrls: ['./list-products.component.scss'],
 })
 export class ListProductsComponent {
+  constructor(private shopService: ShopService) {}
 
+  public products$: Observable<IProduct[]> = this.shopService.getAllProducts();
 }
