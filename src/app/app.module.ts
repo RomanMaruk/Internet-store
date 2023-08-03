a756c1497f9f7559710a03476be8a2685191348b
Roman Maruk <maruchok@gmail.com>
2023-08-03 18:21:11 +0300

    create initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListProductsComponent } from './pages/list-products/list-products.component';
+import { CardProductComponent } from './components/card-product/card-product.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListProductsComponent,
+    CardProductComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
